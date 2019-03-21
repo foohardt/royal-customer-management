a8d13c3b122e06dbc1bfb9743a8525f1afd63a5c
JensErhardt <jens-erhardt@web.de>
2019-03-21 16:19:36 +0100

    Added Route to get query for nachname.

--- Server/routes/costumers.js
+++ Server/routes/costumers.js
@@ -4,27 +4,44 @@ var router = express.Router();
 // Require customer model
 const Customer = require('../models/customer');
 
-// GET list of all customers
+// GET one customer by param "nachname" and if !nachname get list of all
 router.get('/', (req, res, next) => {
-  Customer.find()
-    .then(customers => {
-      res.json(customers)
-    })
-    .catch(err => next(err));
+  if (req.query.nachname) {
+    const lastName = req.query.nachname;
+
+    Customer.findOne({ 'name.last': lastName })
+      .then(customer => {
+        res.json(customer);
+      })
+      .catch(err => next(err));
+  } else {
+    
+    Customer.find()
+      .then(customers => {
+        res.json(customers);
+      })
+      .catch(err => next(err))
+  }
 });
 
 // GET one customer by id 
 router.get('/:id', (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Customer.findById(id)
-      .then(data => {
-        res.json(data)
-      })
-      .catch(err => next(err));
+  Customer.findById(id)
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => next(err));
 });
 
 // GET list of customers by query for lastname  ?nachname=xxx
+
+router.get('/', (req, res, next) => {
+  const lastName = req.query.nachname;
+  console.log("LASTNAME", lastName)
+});
+
 // POST new customer
 // PUT edit customer by id /:id
 // DELETE customer by id /:id
